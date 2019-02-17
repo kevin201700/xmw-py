78a03b0d5b47695e1355881620c0302be2b9c17c
kevin201700 <47049020+kevin201700@users.noreply.github.com>
2019-02-17 11:39:06 +0800

    Add files via upload

---
+++ blpj1.py
@@ -0,0 +1,17 @@
+text = 9*9*9*9*9*9
+text = 285/3-8*6+2
+text = 98*67+21-32/8
+text = (2+4)*108/9
+text = 9**6
+text = 7**8
+text = 5**7
+text = 8**9
+text = '123'
+text = "Welcome to xiaomawang"
+text = "hello "*100
+text = "Welcome to xiaomawang "*100
+text = '1'+'hello'
+text = int(str(1))+10
+text = int('18')+5
+text = "18" + str(5)
+print(text)
